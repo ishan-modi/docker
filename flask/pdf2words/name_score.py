1fc7e4e7d06806c07468fef4f419bfa6604d39f4
ishan-modi <ishan.mce17@sot.pdpu.ac.in>
2021-01-12 19:32:38 +0530

    adding data

--- flask/pdf2words/name_score.py
+++ flask/pdf2words/name_score.py
@@ -1,8 +1,12 @@
 import os
 import json
 from pdf2words import document
+import cv2
+import numpy as np
 import re
 from operator import itemgetter
+from collections import OrderedDict
+
 
 class name_scoring:
     def __init__(self):
@@ -10,10 +14,12 @@ class name_scoring:
         self.clusters = []
         self.score = []
         self.flg = 0
+        self.size=[]
+        self.location=[]
         self.position_x = 0
         self.position_y = 0
 
-    def forming(self,obj):
+    def forming(self, obj):
         word_group = []
 
         d = document.Document()
@@ -23,7 +29,7 @@ class name_scoring:
         height = int(wordpage._height)
         width = int(wordpage._width)
 
-        thresh_y = 0.009 * height
+        thresh_y = 0.008 * height
 
         half_h = height//2
 
@@ -33,13 +39,13 @@ class name_scoring:
         cnt = 0
         flag = 0
 
-        #iterating through all words of 1st page
+         #iterating through all words of 1st page
         for word in wordpage._words:
             x0 = word._x0
             x1 = word._x1
             y0 = word._y0
             y1 = word._y1
-
+    
             # extracting words in top half of first page
 
             if(y1 <= half_h):
@@ -64,16 +70,16 @@ class name_scoring:
                             word_group.append(word)
 
                             flag = 1
-
+                    
                     if(flag >= 2):
 
                         diff2_x = x0 - temp_x
-                        diff2_y = y1 - temp_y
+                        diff2_y = y1 - temp_y1
 
                         #grouping words in particular cluster based on minimum difference
 
                         if(abs(diff2_y) <= thresh_y):
-                            thresh_x= ((y1 + temp_y)/2)*0.3
+                            thresh_x= (((y1-y0) + (temp_y1-temp_y0))/2)*0.55
                             if(abs(diff1_x-diff2_x) <= thresh_x):
                                 word_group.append(word)
                                 diff1_x = diff2_x
@@ -101,7 +107,8 @@ class name_scoring:
                     word_group.append(word)
 
                 temp_x = x1
-                temp_y = y1
+                temp_y1 = y1
+                temp_y0 = y0
                 self.top_words.append(word)
 
                 flag += 1
@@ -110,62 +117,94 @@ class name_scoring:
 
     def clean(self):
         new_clusters = []
+        
+        def splitbydelimeter(symbl,cluster,i):
+            new_cluster=[]
+            if(len(cluster) > i+1):
+                if(cluster[i]._text != symbl):
+                    cluster[i]._text = cluster[i]._text.replace(symbl, '')
+                    temp = cluster[:i]
+                    if(temp!=[]):
+                        new_cluster.append(temp)
+                    temp = cluster[i:]
+                    if(temp!=[]):
+                        new_cluster.append(temp)
+                else:
+                    temp = cluster[:i]
+                    if(temp!=[]):
+                        new_cluster.append(temp)
+                    temp = cluster[i+1:]
+                    if(temp!=[]):
+                        new_cluster.append(temp)
+            else:
+                if(cluster[i]._text != symbl):
+                    cluster[i]._text = cluster[i]._text.replace(symbl, '')
+                    temp = cluster[:i+1]
+                    if(temp!=[]):
+                        new_cluster.append(temp)
+                else:
+                    temp = cluster[:i]
+                    if(temp!=[]):
+                        new_cluster.append(temp)
+            
+            return new_cluster
+
         for cluster in self.clusters:
             temp = []
             flag = 0
-            for i in range(len(cluster)):
-
-                # 1.) removing all clusters with only one word
-
-                if(len(cluster) == 1):
-                    flag = 1
-                    break
-
-                # 1.) keep the cluster that have words with substrings (MR.|Mr.|M/S.|Mrs.)
-
-                if(re.search('(MR.|Mr.|M/S.|Mrs.|Miss.)', cluster[i]._text) is not None):
+            for i in range(len(cluster)): 
+                
+                # keep the cluster that have words with substrings (MR.|Mr.|M/S.|Mrs.)
+                
+                if(re.search('(MR\.|Mr\.|M\/S\.|Mrs\.|Miss.|Dr\.|messrs|Smt\.|S\/O.)', cluster[i]._text) is not None):
                     temp = cluster
                     break
 
-                # 1.) removing words with special characters ' / , ; - ' and
+                # removing words with special characters '  , ; * ' and
                 
-                elif(re.search('[\/;*,]', cluster[i]._text) is not None):
+                elif(re.search('[;*,]', cluster[i]._text) is not None):
                     temp = []
                     flag = 1
                     break
-                    
-                # 1.) remove clusters with words consisting of only numbers of length <= 3
-                # 2.) any word without alphabet (clusters having ' : & -' have been ignored)
 
-                elif(re.search('^[^A-Za-z:&-]+$',cluster[i]._text) is not None):
+                # remove clusters with only numbers of length <= 3
+                
+                elif(re.search('^[^A-Za-z:&-\/]+$',cluster[i]._text) is not None):
                     if(len(cluster[i]._text)<=3):
                         temp = []
                         flag = 1
                         break
                     continue
-
-                # 1.) dealing with words having ' : '
-                #   -- removing every thing before colon from cluster
-                #   -- keeping in cluster words that occur left of colon
-
+                
+                # dealing with words having ' : - /'
+                # every thing before colon in one cluster
+                # every thing before colon in another cluster
+                
                 elif(':' in cluster[i]._text):
-                    cluster[i]._text = cluster[i]._text.replace(':', '')
-                    if(len(cluster) > i+1):
-                        if(cluster[i]._text != ''):
-                            temp = cluster[i:]
-                            new_clusters.append(temp)
-                        else:
-                            temp = cluster[i+1:]
-                            new_clusters.append(temp)
-                    flag = 1
+                    new_clusters += splitbydelimeter(':',cluster,i)
+                    flag=1
                     break
+                
+                elif('/' in cluster[i]._text):
+                    new_clusters+= splitbydelimeter('/',cluster,i)
+                    flag=1
+                    break
+                
+                elif('-' in cluster[i]._text):
+                    new_clusters+= splitbydelimeter('-',cluster,i)
+                    flag=1
+                    break
+                
+                # cluster remains as it is
+
                 else:
                     temp.append(cluster[i])
+      
             if(flag == 0):
                 if(temp!=[]):
                     new_clusters.append(temp)
 
-        self.clusters = new_clusters
+        self.clusters=new_clusters
 
     def read(self, path):
         with open(path, 'r') as f:
@@ -175,45 +214,55 @@ class name_scoring:
         return l
 
     def scoring(self):
+        cluster_size=[]
         for cluster in self.clusters:
-            scr = 0
+            scr = 0    
 
-            cities = self.read('pdf2words/name_score/cities.txt')
+            cities=self.read('pdf2words/name_score/cities.txt')
 
             first_names = self.read('pdf2words/name_score/first_names.txt')
 
             last_names = self.read('pdf2words/name_score/last_names.txt')
+            
+            words = ''.join([x._text for x in cluster])
+            l = [x._text.upper() for x in cluster]
 
             # checking for cluster length
 
             if(len(cluster) >= 3):
-                scr += 3
-
-            words = ''.join([x._text for x in cluster])
-            l = [x._text.upper() for x in cluster]
-
+                scr += 2
+            
             # position based scoring
 
-            x_measure = 0
-            y_measure = 0
-            length = len(cluster)
+            x_measure=0
+            y_measure=0
+            size = 0
+            length=len(cluster)
             for i in range(len(cluster)):
-                y_measure = y_measure+(cluster[i]._y0+cluster[i]._y1)//2
-                if(i == 0):
-                    x_measure += cluster[i]._x0
-                elif(i == length-1):
-                    x_measure += cluster[i]._x1
-                    x_measure = x_measure//2
-            y_measure = y_measure//length
+                size=size+abs(cluster[i]._y0-cluster[i]._y1)
+                y_measure=y_measure+(cluster[i]._y0+cluster[i]._y1)//2
+                if(i==0):
+                    x_measure+=cluster[i]._x0
+                elif(i==length-1):
+                    x_measure+=cluster[i]._x1
+                    x_measure=x_measure//2
+            y_measure=y_measure//length
+            size=size//length
 
-            if(self.position_x >= x_measure and self.position_y >= y_measure):
-                scr += 5
-            elif(self.position_x <= x_measure and self.position_y >= y_measure):
-                scr += 1
-            elif(self.position_x >= x_measure and self.position_y <= y_measure):
-                scr += 1
+            self.size.append(size)
+            self.location.append(y_measure)
+
+            #print(x_measure,y_measure,self.position_x,self.position_y,cluster[0]._text)
+
+            if(self.position_x>=x_measure and self.position_y>=y_measure):
+                scr+=6
+            elif(self.position_x<=x_measure and self.position_y>=y_measure):
+                scr+=2
+            elif(self.position_x>=x_measure and self.position_y<=y_measure):
+                scr+=1
             else:
-                scr += 0
+                scr+=0
+
 
             # checking for cities
 
@@ -224,14 +273,14 @@ class name_scoring:
                     break
 
             if(flag == 1):
-                scr -= 1
-
+                scr -= 2.5
+            
             # checking for firstnames
 
             flag = 0
             for i in first_names:
                 if(i.upper() in l):
-                    flag = 1
+                    flag=1
                     break
 
             if(flag == 1):
@@ -244,25 +293,42 @@ class name_scoring:
                 if(i.upper() in l):
                     flag = 1
                     break
-
+            
             if(flag == 1):
                 scr += 3
 
-            # checking for commonly occuring words
+            # checking for commonly occuring bank words
 
-            common=self.read('pdf2words/name_score/bank_addr_terms.txt')
-
+            common = self.read('pdf2words/name_score/bank_terms.txt')
             flag = 0
             for i in common:
-                flag += sum([1 if i.upper() in x and len(i) >=
-                             4 else 1 if i.upper() == x and len(i) < 4 else 0 for x in l])
+                if(len(i)>=4 and i.upper() in l):
+                    flag+=1
+                else:
+                    for x in l:
+                        if(i.upper()==x):
+                            flag+=1
 
+            if(flag >= 1):
+                scr -= (5*(flag))
+
+            # checking for commonly occuring bank words
+            
+            common = self.read('pdf2words/name_score/addr_terms.txt')
+            flag = 0
+            for i in common:
+                if(len(i)>=4 and i.upper() in l):
+                    flag+=1
+                else:
+                    for x in l:
+                        if(i.upper()==x):
+                            flag+=1
             if(flag >= 1):
                 scr -= (5*(flag))
 
             # checking for company suffix
 
-            company=self.read('pdf2words/name_score/company_suffix.txt')
+            company = self.read('pdf2words/name_score/company_suffix.txt')
 
             flag = 0
             for i in company:
@@ -270,24 +336,66 @@ class name_scoring:
                     flag += 1
 
             if(flag >= 1):
-                scr += (1.5*(flag))
+                scr += 3
 
             # checking for numeric values
 
             if(re.search('[0-9]', words) is not None):
                 scr -= 5
 
+            # checking clusters with only one word
+
+            if(len(cluster) == 1):
+                scr-=3
+
             # checking for case sensitive
 
             if(words.isupper()):
                 scr += 1
 
-            if(re.search('(MR.|Mr.|M/S.|Mrs.)', words) is not None):
+            if(re.search('(MR\.|Mr\.|M\/S\.|Mrs\.|Miss.|Dr\.|messrs|Smt\.)', words) is not None):
                 scr += 3
-
+            
             self.score.append(scr)
+        
+        # processing top 5 clusters for more accuracy
 
-    def print_c(self,clusters):
+        index=np.argsort(np.asarray(self.score))
+        index=index[::-1]
+        index=index[:5]
+
+        location={}
+        size={}
+        for i in index:
+            size[self.size[i]]=i
+            location[self.location[i]]=i
+        
+        size=OrderedDict(sorted(size.items(),reverse=True))
+        location=OrderedDict(sorted(location.items()))
+
+        # 1.) size based scoring
+        
+        cnt=2.5
+        if(len(size)==5):
+            for i in size:
+                if(cnt==2.5):
+                    temp=i
+                    self.score[size[i]]+=cnt
+                else:
+                    if(abs(temp-i)>=thresh_y):
+                        cnt-=0.5
+                        self.score[size[i]]+=cnt
+                    else:
+                        self.score[size[i]]+=cnt
+                        
+        # 2.) location based scoring
+
+        cnt=2.5
+        for i in location:
+            self.score[location[i]]+=cnt
+            cnt-=0.5
+
+    def print_c(self, clusters):
         for cluster in clusters:
             for words in cluster:
                 print(words._text+' ', end='')
@@ -297,13 +405,13 @@ class name_scoring:
         Z = [[scr, cluster] for scr, cluster in zip(self.score, self.clusters)]
         Z = list(reversed(sorted(Z, key=itemgetter(0))))
         clusters = [i[1] for i in Z[:1]]
-        name=''
+        name = ''
         for words in clusters[0]:
-            name+=words._text+' '
+            name += words._text+' '
 
         return name
 
-    def check_acc(self,json_path):
+    def check_acc(self, json_path):
         # reverse sorting clusters based on score
         Z = [[scr, cluster] for scr, cluster in zip(self.score, self.clusters)]
         Z = list(reversed(sorted(Z, key=itemgetter(0))))
@@ -332,6 +440,11 @@ class name_scoring:
 
             if(dict[json_path.split('.')[0]] != 'None'):
                 self.flg = 1
+                # original name is a substring of found words
                 if(dict[json_path.split('.')[0]].upper() in words.upper()):
+                    self.flg = 2
+                    break
+                # 2 or more found words occur in original
+                if(sum([1 if(word._text.upper() in dict[json_path.split('.')[0]].upper()) else 0 for word in cluster]) >= 2):
                     self.flg = 2
                     break
